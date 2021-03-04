7f7fa51655477a1dd3af467ea0207f6eba91f133
matheusleanon <matheus@leanonsystems.com>
2021-03-04 04:13:21 -0300

    chore: setup to work on mac and setup build

--- Laboratorio 1/quadrado.cpp
+++ Laboratorio 1/quadrado.cpp
@@ -1,6 +1,13 @@
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#endif
+
 #include <stdio.h>
 #include <iostream>
 #define TAMANHO_JANELA 500
